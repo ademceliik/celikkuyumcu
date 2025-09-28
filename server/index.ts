c794b60af2286abbc35a443d6cb468093ab34ed6
ademceliik <adem.chelik.9@gmail.com>
2025-09-28 22:23:24 +0300

    vite defined

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
 import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { setupVite, serveStatic, log } from "./vite-start";
 import { initializeDatabase } from "./db";
 
 const app = express();
