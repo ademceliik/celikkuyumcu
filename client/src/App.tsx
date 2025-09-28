4bc27b95e714ff5cd1458fdae8dc59cb9949ea3e
ademceliik <adem.chelik.9@gmail.com>
2025-09-28 23:34:26 +0300

    fix page redirect 4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route } from "wouter";
-import { HashRouter } from "./HashRouter";
+import HashRouter from "./HashRouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
